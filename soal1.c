1b426fd5a83837526af96b73345161b0c9a1f2cc
son <geraldosilalahi72@gmail.com>
2026-04-14 08:59:35 +0700

    Enhance soal1.c with comments and color logic
    Add header comments and modify main function to include color coding based on conditions.

--- soal1.c
+++ soal1.c
@@ -1,5 +1,32 @@
-#include <stdio.h>
+/** EL2008 Praktikum Pemecahan Masalah dengan Pemrograman 2024/2025
+ *   Modul               : 01 Overview of C Language
+ *   Hari dan Tanggal    : Selasa 14 April 2026
+ *   Nama (NIM)          : Geraldo Son Luther Silalahi (13224018)
+ *   Nama File           : soal1.c
+ *   Deskripsi           : Soal1 - Menara Lampu
+ * 
+ */
 
-int main(void) {
+
+ #include <stdio.h>
+ #include <string.h>
+
+int main (){
+    int i, N;
+    scanf("%d", &N);
+    for (i = 0; i <= N; i++){
+        if (i % 2 == 0 && i % 3 == 0 ){
+            printf("BIRU\n");
+        } else if (i % 3 == 0){
+            printf("MERAH\n");
+        } else if (i % 2 == 0){
+            printf("KUNING\n");
+        } else if (i >= 10 && i % 2 == 0){
+            printf("HIJAU\n");
+        } else{
+            printf ("%d\n", i);
+        }
+    }
+
     return 0;
 }
